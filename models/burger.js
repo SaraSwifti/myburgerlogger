8f56b531f6d02aad9d94edb946e79b6ff8e87fd0
Sara Taylor <sara.swifti@gmail.com>
2021-03-08 11:10:12 -0500

    models and config

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+
+* Inside `burger.js`, import `orm.js` into `burger.js`
+
+* Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+
+* Export at the end of the `burger.js` file.
+
+const orm = require('../config/orm.js');
+
+const burger = {
+  all(cb) {
+    orm.all('burgers', (res) => cb(res));
+  },
+  // The variables cols and vals are arrays.
+  create(cols, vals, cb) {
+    orm.create('burgers', cols, vals, (res) => cb(res));
+  },
+  update(objColVals, condition, cb) {
+    orm.update('burgers', objColVals, condition, (res) => cb(res));
+  },
+  delete(condition, cb) {
+    orm.delete('burgers', condition, (res) => cb(res));
+  },
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
